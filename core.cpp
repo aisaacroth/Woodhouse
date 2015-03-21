6fc59840c6638f96732f3903ef2e381fe03a2a71
aisaacroth <aisaacroth@gmail.com>
2015-03-21 19:33:53 +0900

    Working on core stuff. Need to make it more object-oriented.

---
+++ core.cpp
@@ -0,0 +1,8 @@
+#include "core.hpp"
+
+/**
+ * Implementation information for the Jarvis User interface
+ *
+ * Author: Alexander Roth
+ * Date:   2015-03-21
+ **/
---
+++ core.hpp
@@ -0,0 +1,11 @@
+#ifndef __CORE_HPP__
+#define __CORE_HPP__
+
+/**
+ * Header file that contains declaration information for Jarvis user interface.
+ *
+ * Author: Alexander Roth
+ * Date:   2015-03-21
+ **/
+
+#endif
